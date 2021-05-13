5e8f641d9da1f77249dc29ee2b11bc925768cffa
Andrey Draganchuk <spirifex@gmail.com>
2021-05-13 13:21:58 +0400

    Docker refactoring

--- api/public/index.php
+++ api/public/index.php
@@ -2,12 +2,10 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-echo '{}';
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
