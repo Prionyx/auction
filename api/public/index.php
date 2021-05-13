03df96a431437ef06dd5df0d1d11f8fbc22c916e
Andrey Draganchuk <spirifex@gmail.com>
2021-05-13 22:58:21 +0400

    Rewrite to PSR request handler

--- api/public/index.php
+++ api/public/index.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use App\Http\Action\HomeAction;
 use DI\ContainerBuilder;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ResponseFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -14,7 +16,8 @@ $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions([
     'config' => [
         'debug' => (bool)getenv('APP_DEBUG'),
-    ]
+    ],
+    ResponseFactoryInterface::class => Di\get(ResponseFactory::class),
 ]);
 $container = $containerBuilder->build();
 $app = AppFactory::createFromContainer($container);
--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -4,21 +4,29 @@ namespace App\Http\Action;
 
 use App\Http\Http;
 use JsonException;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use stdClass;
 
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
+    private ResponseFactoryInterface $factory;
+
+    public function __construct(ResponseFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
     /**
-     * @param Request $request
-     * @param Response $response
-     * @param array $args
-     * @return Response
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
      * @throws JsonException
      */
-    public function __invoke(Request $request, Response $response, array $args): Response
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $response = $this->factory->createResponse();
         return Http::json($response, new stdClass());
     }
 }
