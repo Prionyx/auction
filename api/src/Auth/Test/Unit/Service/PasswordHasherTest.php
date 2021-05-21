ccc289f3f5923e5f667080e64168e0426644c904
Andrey Draganchuk <spirifex@gmail.com>
2021-05-21 17:51:03 +0400

    Improve hasher tests

--- api/src/Auth/Test/Unit/Service/PasswordHasherTest.php
+++ api/src/Auth/Test/Unit/Service/PasswordHasherTest.php
@@ -15,7 +15,7 @@ class PasswordHasherTest extends TestCase
 {
     public function testHash(): void
     {
-        $hasher = new PasswordHasher();
+        $hasher = new PasswordHasher(16);
 
         $hash = $hasher->hash($password = 'new-password');
 
@@ -25,7 +25,7 @@ class PasswordHasherTest extends TestCase
 
     public function testHashEmpty(): void
     {
-        $hasher = new PasswordHasher();
+        $hasher = new PasswordHasher(16);
 
         $this->expectException(InvalidArgumentException::class);
         $hasher->hash('');
